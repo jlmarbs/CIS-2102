2cf1e94a77a7ecddec079cfa8cee30727ff0950c
jlmarbs <130033706+jlmarbs@users.noreply.github.com>
2023-10-14 20:05:01 +0800

    Added Contact Book Activity

---
+++ ContactForm/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css'; 
+import ContactBook from './Components/ContactBook';
+function App() {
+  return (
+    <div className="App">
+      <ContactBook />
+    </div>
+  );
+}
+
+export default App
